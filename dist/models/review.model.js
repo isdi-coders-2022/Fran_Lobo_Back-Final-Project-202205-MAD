b5d678c7b4a3f345ef3c80a6a1a3149244ecf8c3
FranLobo <floboflores@gmail.com>
2022-07-05 14:08:52 +0200

    Solving problems with git

--- dist/models/review.model.js
+++ dist/models/review.model.js
@@ -1,18 +1,14 @@
 import mongoose from 'mongoose';
 import { mongooseConnect } from '../db/mongoose.js';
-
+import { SchemaTypes } from 'mongoose';
 await mongooseConnect();
 const reviewSchema = new mongoose.Schema({
     idUser: {
-        type: [
-            { type: mongoose.SchemaTypes.ObjectId, ref: 'User', default: null },
-        ],
+        type: [{ type: SchemaTypes.ObjectId, ref: 'User', default: null }],
         required: true,
     },
     idGame: {
-        type: [
-            { type: mongoose.SchemaTypes.ObjectId, ref: 'Game', default: null },
-        ],
+        type: [{ type: SchemaTypes.ObjectId, ref: 'Game', default: null }],
         required: true,
     },
     text: {
