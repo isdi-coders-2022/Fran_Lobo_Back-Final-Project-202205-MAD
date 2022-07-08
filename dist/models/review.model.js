e92df114e098342721622a2b15292ab390a4379e
FranLobo <floboflores@gmail.com>
2022-07-08 15:35:00 +0200

    Fixed faulty import

--- dist/models/review.model.js
+++ dist/models/review.model.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { mongooseConnect } from '../db/mongoose';
+import { mongooseConnect } from '../db/mongoose.js';
 (async () => {
     await mongooseConnect();
 })();
