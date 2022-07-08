b5c0c1c7158617d8a1791df06d58e611c7eed8f1
FranLobo <floboflores@gmail.com>
2022-07-08 17:35:53 +0200

    Finish testing review

--- dist/models/review.model.js
+++ dist/models/review.model.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 import { mongooseConnect } from '../db/mongoose.js';
-<<<<<<< Updated upstream
 (async () => {
     await mongooseConnect();
 })();
@@ -8,17 +7,12 @@ const reviewSchema = new mongoose.Schema({
     idUser: {
         type: mongoose.SchemaTypes.ObjectId,
         ref: 'User',
-=======
-import { SchemaTypes } from 'mongoose';
-await mongooseConnect();
-const reviewSchema = new mongoose.Schema({
-    idUser: {
-        type: [{ type: SchemaTypes.ObjectId, ref: 'User', default: null }],
->>>>>>> Stashed changes
         required: true,
     },
     idGame: {
-        type: [{ type: SchemaTypes.ObjectId, ref: 'Game', default: null }],
+        type: [
+            { type: mongoose.SchemaTypes.ObjectId, ref: 'Game', default: null },
+        ],
         required: true,
     },
     text: {
